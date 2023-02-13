91d5595cd0589a44e2a0a91ab681d2ef6d445916
Aayam <aayam.oza@gmail.com>
2023-02-13 22:18:04 +0545

    strength slider, and switches made. Next: Changing enable btn

--- packages/athena-vision-web/src/app/home-view/home-view.tsx
+++ packages/athena-vision-web/src/app/home-view/home-view.tsx
@@ -1,13 +1,16 @@
 import styles from './home-view.module.scss';
 import { clsx } from 'clsx';
-import { useRef, useState } from 'react';
+import { useState, ChangeEvent } from 'react';
 
 /* eslint-disable-next-line */
 export interface HomeViewProps {}
 
 export function HomeView(props: HomeViewProps) {
-  const sliderRef = useRef(null);
-  const [strengthSliderState, setStrengthSliderState] = useState('1');
+  const [strengthState, setStrengthState] = useState('1');
+
+  const onStrengthChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setStrengthState((_) => e.target.value);
+  };
 
   return (
     <div className={styles['container']}>
@@ -22,43 +25,67 @@ export function HomeView(props: HomeViewProps) {
         <button className={clsx(styles['enable-btn'], 'Typography-1')}>
           Enable
         </button>
+
         {/* Adjust Form */}
-        <div>
-          <form>
-            <div className={styles['strength-slider-container']}>
-              <label className="Typography-3">Strength: </label>
-              <div className={styles['strength-slider-and-labels']}>
-                <div className={styles['strength-slider-dot-wrapper']}>
-                  <input
-                    ref={sliderRef}
-                    type="range"
-                    className={styles['strength-slider']}
-                    name="strength"
-                    id="strength"
-                    min={1}
-                    max={3}
-                    value={strengthSliderState}
-                    onChange={(e) =>
-                      setStrengthSliderState((_) => e.target.value)
-                    }
-                  />
-
-                  <div className={styles['dots']}>
-                    <div className={styles['dot']}>.</div>
-                    <div className={styles['dot']}>.</div>
-                    <div className={styles['dot']}>.</div>
-                  </div>
-                </div>
-
-                <div className={styles['level-labels']}>
-                  <span>Low</span>
-                  <span>Mid</span>
-                  <span>High</span>
-                </div>
-              </div>
+        <form>
+          <div className={styles['strength-slider-container']}>
+            <h1 className={clsx(styles['filter-title'], 'Typography-5')}>
+              Strength
+            </h1>
+            <div id={styles['strength-amount-slider']}>
+              <input
+                type="radio"
+                name="strength-amount"
+                id="low"
+                value="0"
+                checked={strengthState === '0'}
+                onChange={onStrengthChange}
+                required
+              />
+              <label htmlFor="low" data-strength-amount="Low"></label>
+              <input
+                type="radio"
+                name="strength-amount"
+                id="mid"
+                value="1"
+                checked={strengthState === '1'}
+                onChange={onStrengthChange}
+                required
+              />
+              <label htmlFor="mid" data-strength-amount="Mid"></label>
+              <input
+                type="radio"
+                name="strength-amount"
+                id="high"
+                value="2"
+                checked={strengthState === '2'}
+                onChange={onStrengthChange}
+                required
+              />
+              <label htmlFor="high" data-strength-amount="High"></label>
+              <div id={styles['strength-amount-pos']}></div>
             </div>
-          </form>
-        </div>
+          </div>
+
+          <div className={styles['etone-switch-container']}>
+            <h1 className={clsx(styles['filter-title'], 'Typography-5')}>
+              Emotional Tone
+            </h1>
+            <div className={styles['etone-switch']}>
+              <label className={styles['label']}>
+                <div className={styles['toggle']}>
+                  <input
+                    className={styles['toggle-state']}
+                    type="checkbox"
+                    name="check"
+                    value="check"
+                  />
+                  <div className={styles['indicator']}></div>
+                </div>
+              </label>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
