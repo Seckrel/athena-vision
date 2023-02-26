9dbd1a166a91e15b51a250bee963f1245b4b1180
Aayam <aayam.oza@gmail.com>
2023-02-26 19:19:20 +0545

    postinstall now ask yes or no to install python libs

--- py-libs/setup.py
+++ py-libs/setup.py
@@ -1,8 +1,27 @@
 import os
+import curses
 from sharedLibs.find import find_pipfile
+from sharedLibs.print_pretty import print_blue_info
+
+
+def ask_whether_to_install() -> bool:
+    '''
+    Ask users wether to install pip packages or not
+    '''
+    ask_var: str = input('Do you want to install python packages(y/N): ')
+    if (ask_var.lower() == 'y'):
+        return True
+    return False
 
 
 def main():
+    setup_flag = ask_whether_to_install()
+
+    if not setup_flag:
+        print_blue_info('''If you want to install in future
+        `yarn pipenv install`''')
+        return
+
     os.chdir(find_pipfile())
     os.system("pipenv install")
     os.system("pipenv install django")
