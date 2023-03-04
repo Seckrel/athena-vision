b526a07fd309f6d399dda019f1537935248b3a0a
Aayam <aayam.oza@gmail.com>
2023-03-04 19:55:43 +0545

    fix: infinite scrolling is working with tweet sample stream and hate speech prediction

--- packages/athena-vision-api/backend/twitterstream/consumers.py
+++ packages/athena-vision-api/backend/twitterstream/consumers.py
@@ -15,6 +15,7 @@ class TweetStreamConsumer(WebsocketConsumer):
         data = json.loads(text_data)
         if data["signal"].lower() == "more":
             self.__getStream()
+            self.__makePrediction()
 
     def disconnet(self, close_code):
         pass
@@ -28,11 +29,7 @@ class TweetStreamConsumer(WebsocketConsumer):
         print("--------->Samping")
         stream.sample(expansions=["author_id"], tweet_fields=[
             "entities", "lang"])
-
-        # self.send(text_data=json.dumps({
-        #     "type": "json",
-        #     "data": self.tweet_data
-        # }))
+        print("-------->Sampled")
 
     def __makePrediction(self):
         model = Prediction()
@@ -41,6 +38,8 @@ class TweetStreamConsumer(WebsocketConsumer):
         for idx, tweet in enumerate(self.tweet_data):
             hate_flag = model.predict_hate(tweet["text"])
             self.tweet_data[idx]["hate_flag"] = str(hate_flag[0])
+
+        print("="*4, ">Predicting<", "="*4)
 
         self.send(text_data=json.dumps({
             "type": "json",
